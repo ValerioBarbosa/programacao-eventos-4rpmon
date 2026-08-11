743fcc1a618cdaf1ac4a23087557298d7efdd052
Valério Barbosa <valerio.direito@gmail.com>
2026-08-11 15:52:27 -0300

    fix: simplifica campo de data dos eventos (#11)
    Substitui os campos Data Inicial e Data Final por um único campo Data, preserva compatibilidade interna e aceita horários que atravessam a meia-noite.

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "agenda-4rpmon-v4";
+const CACHE_NAME = "agenda-4rpmon-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./admin.html",
   "./style.css?v=20260809-3",
-  "./app.js?v=20260810-4",
+  "./app.js?v=20260811-1",
   "./firebase-config.js",
   "./manifest.webmanifest",
   "./image.png",
