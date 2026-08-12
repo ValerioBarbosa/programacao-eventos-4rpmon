ee773ac0701e5ce4e8317cfb4ae53e1a3913e916
Valério Barbosa <valerio.direito@gmail.com>
2026-08-12 16:05:42 -0300

    feat: melhora o PDF da programação operacional (#14)
    Cria PDF institucional em A4 com eventos agrupados por data, resumo por esquadrão, cartões estruturados, paginação e continuidade entre páginas.

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "agenda-4rpmon-v7";
+const CACHE_NAME = "agenda-4rpmon-v8";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./admin.html",
   "./style.css?v=20260812-1",
-  "./app.js?v=20260812-1",
+  "./app.js?v=20260812-2",
   "./firebase-config.js",
   "./manifest.webmanifest",
   "./image.png",
